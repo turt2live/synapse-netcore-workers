55dd56078f49689758d6a54adca5f70180ce254c
Travis Ralston <travpc@gmail.com>
2019-02-19 19:41:47 -0700

    Add a generic transaction handler for use by workers

---
+++ Matrix.SynapseInterop.Common/Transactions/Transaction.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Matrix.SynapseInterop.Common.Transactions
+{
+    public class Transaction<T> where T : class
+    {
+        private List<T> _elements = new List<T>();
+
+        public ICollection<T> Elements { get => _elements.AsReadOnly(); }
+
+        public string Id { get; private set; }
+
+        public TransactionStatus Status { get; internal set; }
+
+        public Transaction(string id) : this(id, new T[0]) { }
+
+        public Transaction(string id, ICollection<T> items)
+        {
+            this.Id = id;
+            this.AddItems(items);
+        }
+
+        internal void AddItems(ICollection<T> items)
+        {
+            if (Status != TransactionStatus.NEW)
+                throw new InvalidOperationException("Cannot modify a transaction which is not new");
+            
+            _elements.AddRange(items);
+        }
+    }
+}
