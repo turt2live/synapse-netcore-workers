05ced1c2b65468301202f0ab2d4789be688b7b29
Travis Ralston <travpc@gmail.com>
2019-02-19 19:41:47 -0700

    Add a generic transaction handler for use by workers

---
+++ Matrix.SynapseInterop.Common/Transactions/TransactionManager.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Matrix.SynapseInterop.Common.Transactions
+{
+    public abstract class TransactionManager<T> where T:class
+    {
+        private static Random RANDOM = new Random();
+
+        private Dictionary<string, Transaction<T>> _transactions = new Dictionary<string, Transaction<T>>();
+        private List<Transaction<T>> _queuedTransactions = new List<Transaction<T>>();
+
+        private int _maxElements;
+        private bool _storeSent;
+
+        public TransactionManager(int maxElementsPerTransaction = 50, bool storeSentTransactionsInMemory=true)
+        {
+            _maxElements = maxElementsPerTransaction;
+            _storeSent = storeSentTransactionsInMemory;
+
+            List<Transaction<T>> pending = LoadTransactionQueue();
+            foreach(Transaction<T> txn in pending)
+            {
+                _queuedTransactions.Add(txn);
+                _transactions[txn.Id] = txn;
+            }
+
+            ICollection<Transaction<T>> buffers = LoadTransactionWithStatus(TransactionStatus.NEW);
+            foreach (Transaction<T> txn in buffers) _transactions[txn.Id] = txn;
+        }
+
+        /// <summary>
+        /// Persists a transaction to a data store. This can happen when a transaction's elements
+        /// change, or its status changes. New transactions will also be run through this.
+        /// </summary>
+        /// <param name="transaction">The transaction to persist</param>
+        protected abstract void PersistTransaction(Transaction<T> transaction);
+
+        /// <summary>
+        /// Persists the queue of transactions to send
+        /// </summary>
+        /// <param name="queue">The transactions to send</param>
+        protected abstract void PersistTransactionQueue(List<Transaction<T>> queue);
+
+        /// <summary>
+        /// Loads the queue of transactions to send to the destination. If no transactions are queued,
+        /// this should return an empty queue.
+        /// </summary>
+        /// <returns>The queue of transactions to send. May be empty.</returns>
+        protected abstract List<Transaction<T>> LoadTransactionQueue();
+
+        /// <summary>
+        /// Loads the transactions with a given status, returning an empty collection if none match the criteria.
+        /// </summary>
+        /// <param name="status">The status to look for</param>
+        /// <returns>The transactions matching the criteria. May be empty.</returns>
+        protected abstract ICollection<Transaction<T>> LoadTransactionWithStatus(TransactionStatus status);
+
+        /// <summary>
+        /// Loads a transaction by ID. If the transaction is not found, this should return null.
+        /// </summary>
+        /// <param name="transactionId">The transaction ID to load</param>
+        /// <returns>The transaction, or null if it is not found</returns>
+        protected abstract Transaction<T> LoadTransaction(string transactionId);
+
+        protected string GetNextId()
+        {
+            string proposedId = null;
+
+            do
+            {
+                // Source: https://stackoverflow.com/a/1344242
+                int length = 12;
+                const string chars = "abcdefghijklmnopqrstuvwxyz0123456789";
+                string randomBits = new string(Enumerable.Repeat(chars, length).Select(s => s[RANDOM.Next(s.Length)]).ToArray());
+
+                proposedId = DateTime.Now.ToBinary() + "_" + randomBits;
+            } while (this.GetTransaction(proposedId) != null);
+
+            return proposedId;
+        }
+
+        /// <summary>
+        /// Retrieves a transaction.
+        /// </summary>
+        /// <param name="id">The transaction ID to find</param>
+        /// <returns>The transaction, or null if not found</returns>
+        public Transaction<T> GetTransaction(string id)
+        {
+            if (!_transactions.ContainsKey(id))
+            {
+                Transaction<T> txn = this.LoadTransaction(id);
+                if (txn.Id != id) throw new InvalidProgramException("Loaded transaction that didn't match the one requested");
+                if (txn.Status != TransactionStatus.SENT || _storeSent) _transactions[id] = txn;
+            }
+
+            return _transactions[id];
+        }
+
+        /// <summary>
+        /// Gets the next transaction that should be sent to the destination.
+        /// </summary>
+        /// <returns>The next transaction that should be sent, or null if none ready</returns>
+        public Transaction<T> GetTransactionToSend()
+        {
+            if (_queuedTransactions.Any())
+                return _queuedTransactions[0];
+
+            RotatePendingTransaction();
+            if (_queuedTransactions.Any())
+                return _queuedTransactions[0];
+
+            return null;
+        }
+
+        public void FlagSent(Transaction<T> transaction)
+        {
+            transaction.Status = TransactionStatus.SENT;
+            PersistTransaction(transaction);
+            if (_queuedTransactions.Contains(transaction))
+            {
+                _queuedTransactions.Remove(transaction);
+                PersistTransactionQueue(_queuedTransactions.Clone());
+            }
+
+            if (!_storeSent && _transactions.ContainsKey(transaction.Id))
+                _transactions.Remove(transaction.Id);
+        }
+
+        public void QueueElements(ICollection<T> elements)
+        {
+            Transaction<T> buffer = GetBufferedTransaction();
+            buffer.AddItems(elements);
+        }
+
+        private Transaction<T> GetBufferedTransaction()
+        {
+            Transaction<T> pendingTxn = _transactions.Values.FirstOrDefault(t => t.Status == TransactionStatus.NEW);
+            if (pendingTxn == null) return CreateTransaction();
+            return pendingTxn;
+        }
+
+        private void RotatePendingTransaction()
+        {
+            Transaction<T> pendingTxn = _transactions.Values.FirstOrDefault(t => t.Status == TransactionStatus.NEW);
+            if (pendingTxn == null) return;
+            if (!pendingTxn.Elements.Any()) return;
+
+            pendingTxn.Status = TransactionStatus.QUEUED;
+            PersistTransaction(pendingTxn);
+            _queuedTransactions.Add(pendingTxn);
+            PersistTransactionQueue(_queuedTransactions.Clone());
+
+            CreateTransaction();
+        }
+
+        private Transaction<T> CreateTransaction()
+        {
+            string nextId = GetNextId();
+            Transaction<T> txn = new Transaction<T>(nextId);
+            PersistTransaction(txn);
+            _transactions[nextId] = txn;
+
+            return txn;
+        }
+    }
+}
