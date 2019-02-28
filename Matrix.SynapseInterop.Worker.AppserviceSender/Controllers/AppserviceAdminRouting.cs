25fe1b9941441779ea3f84e6ea83f07aaf92a015
Travis Ralston <travpc@gmail.com>
2019-02-27 22:57:57 -0700

    Quick test of regex path matching

--- Matrix.SynapseInterop.Worker.AppserviceSender/Controllers/AppserviceAdminRouting.cs
+++ Matrix.SynapseInterop.Worker.AppserviceSender/Controllers/AppserviceAdminRouting.cs
@@ -1,4 +1,5 @@
-﻿using Newtonsoft.Json.Linq;
+﻿using System.Text.RegularExpressions;
+using Newtonsoft.Json.Linq;
 using Routable;
 using Routable.Kestrel;
 
@@ -11,6 +12,9 @@ namespace Matrix.SynapseInterop.Worker.AppserviceSender.Controllers
         ) : base(options)
         {
             Add(_ => _.Get("/_matrix/admin/r0/appservices").Try(OnListAppservices));
+
+            Add(_ => _.Put().Path(new Regex("/_matrix/admin/r0/appservices/(?<appserviceId>.*)"))
+                      .Try(OnUpsertAppservice));
         }
 
         private bool OnListAppservices(KestrelRoutableContext ctx,
@@ -25,6 +29,39 @@ namespace Matrix.SynapseInterop.Worker.AppserviceSender.Controllers
                 TestAppservice = new
                 {
                     enabled = true,
+                    as_token = "sample_as_token",
+                    hs_token = "sample_hs_token",
+                    url = "http://localhost:9000",
+                    sender_localpart = "_example",
+                    namespaces = new
+                    {
+                        users = new object[]
+                        {
+                            new {exclusive = true, regex = "@_example.*"}
+                        },
+                        aliases = new object[0],
+                        rooms = new object[0]
+                    }
+                }
+            }));
+
+            return true;
+        }
+
+        private bool OnUpsertAppservice(KestrelRoutableContext ctx,
+                                        KestrelRoutableRequest req,
+                                        KestrelRoutableResponse resp
+        )
+        {
+            resp.ContentType = "application/json";
+
+            resp.Write(JObject.FromObject(new
+            {
+                TestAppservice = new
+                {
+                    enabled = true,
+                    id = req.Parameters["appserviceId"],
+                    //body = req.Body,
                     as_token = "sample_as_token",
                     hs_token = "sample_hs_token",
                     url = "http://localhost:9000",
