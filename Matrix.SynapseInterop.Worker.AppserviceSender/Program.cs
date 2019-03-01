471414c406e80964980b71fe9a23f535feae1241
Will Hunt <will@half-shot.uk>
2019-03-01 14:48:20 +0000

    Drop newest appservice changes
    Signed-off-by: Will Hunt <will@half-shot.uk>

--- Matrix.SynapseInterop.Worker.AppserviceSender/Program.cs
+++ Matrix.SynapseInterop.Worker.AppserviceSender/Program.cs
@@ -1,45 +1,12 @@
-﻿using Matrix.SynapseInterop.Worker.AppserviceSender.Controllers;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Newtonsoft.Json.Linq;
-using Routable;
-using Routable.Kestrel;
-using System;
-using System.Net;
+﻿using System;
 
 namespace Matrix.SynapseInterop.Worker.AppserviceSender
 {
-    internal class Program
+    class Program
     {
-        private static IConfiguration _config;
-        private static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Starting appservice sender...");
-            _config = new ConfigurationBuilder()
-                .AddJsonFile("appsettings.default.json", true, true)
-                .AddEnvironmentVariables()
-                .AddCommandLine(args)
-                .Build();
-
-            var kestrelConfig = _config.GetSection("Kestrel");
-            var host = new WebHostBuilder()
-                .UseKestrel(options => options.Listen(IPAddress.Parse(kestrelConfig.GetValue<string>("bindHost")), kestrelConfig.GetValue<int>("bindPort")))
-                .Configure(builder => builder.UseRoutable(options => options
-                    .WithJsonSupport()
-                    .AddRouting(new AppserviceAdminRouting(options))
-                    .OnError(new KestrelRouting(options)
-                    {
-                        _ => _.Do((context, request, response) =>
-                        {
-                            response.Status = 500;
-                            response.ContentType = "application/json";
-                            response.Write(JObject.FromObject(new {error="Internal server error", errcode="M_UNKNOWN" }));
-                        })
-                    })
-                ))
-                .Build();
-
-            host.Run();
+            Console.WriteLine("Hello World!");
         }
     }
 }
