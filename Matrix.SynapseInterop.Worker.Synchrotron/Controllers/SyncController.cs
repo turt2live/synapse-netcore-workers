7e34f28109cce0fc578839d7239f29f8a0f2cc4f
Will Hunt <will@half-shot.uk>
2019-03-11 18:40:39 +0000

    Add sync

---
+++ Matrix.SynapseInterop.Worker.Synchrotron/Controllers/SyncController.cs
@@ -0,0 +1,19 @@
+﻿using Routable;
+using Routable.Kestrel;
+
+namespace Matrix.SynapseInterop.Worker.Synchrotron.Controllers
+{
+    public class SyncController : KestrelRouting
+    {
+        public AppserviceAdminRouting(
+            RoutableOptions<KestrelRoutableContext, KestrelRoutableRequest, KestrelRoutableResponse> options
+        ) : base(options)
+        {
+            Add(_ => _.Get("/_matrix/admin/r0/appservices").Try(OnListAppservices));
+
+            Add(_ => _.Put().Path(new Regex("/_matrix/admin/r0/appservices/(?<appserviceId>.*)"))
+                      .Try(OnUpsertAppservice));
+        }
+
+    }
+}
---
+++ Matrix.SynapseInterop.Worker.Synchrotron/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Matrix.SynapseInterop.Common;
+using Microsoft.Extensions.Configuration;
+
+namespace Matrix.SynapseInterop.Worker.Synchrotron
+{
+    internal class Program
+    {
+        private static IConfiguration _config;
+
+        private static void Main(string[] args)
+        {
+            _config = new ConfigurationBuilder()
+                     .AddJsonFile("appsettings.default.json", true, true)
+                     .AddJsonFile("appsettings.json", true, true)
+                     .AddEnvironmentVariables()
+                     .AddCommandLine(args)
+                     .Build();
+
+            Logger.Setup(_config.GetSection("Logging"));
+
+            var metricConfig = _config.GetSection("Metrics");
+
+            if (metricConfig != null && metricConfig.GetValue<bool>("enabled"))
+                WorkerMetrics.StartMetrics("federation_worker",
+                                           metricConfig.GetValue("bindPort", 9150),
+                                           metricConfig.GetValue<string>("bindHost"));
+            
+            Console.ReadKey(true);
+        }
+    }
+}
