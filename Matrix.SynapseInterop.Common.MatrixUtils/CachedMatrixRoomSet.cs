f551fe1f37925aabbe3c179e6a4f9b8bcd36a856
Will Hunt <will@half-shot.uk>
2019-03-24 01:30:07 +0000

    Allow more advanced event fetching from the room cache

--- Matrix.SynapseInterop.Common.MatrixUtils/CachedMatrixRoomSet.cs
+++ Matrix.SynapseInterop.Common.MatrixUtils/CachedMatrixRoomSet.cs
@@ -2,6 +2,7 @@
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
+using Matrix.SynapseInterop.Common.Extensions;
 using Matrix.SynapseInterop.Database;
 using Matrix.SynapseInterop.Database.SynapseModels;
 
@@ -18,7 +19,7 @@ namespace Matrix.SynapseInterop.Common.MatrixUtils
             _userMembershipComplete = new HashSet<string>();
         }
         
-        public CachedMatrixRoom GetRoom(string roomId)
+        public CachedMatrixRoom GetRoom(string roomId, bool populateMemberCache = true)
         {
             if (_dict.TryGetValue(roomId, out var room))
             {
@@ -29,7 +30,10 @@ namespace Matrix.SynapseInterop.Common.MatrixUtils
             WorkerMetrics.ReportCacheMiss("CachedMatrixRoomSet.GetRoom");
 
             room = new CachedMatrixRoom(roomId);
-            room.PopulateMemberCache();
+
+            if (populateMemberCache)
+                room.PopulateMemberCache();
+
             _dict.TryAdd(roomId, room);
             return room;
         }
@@ -46,11 +50,10 @@ namespace Matrix.SynapseInterop.Common.MatrixUtils
 
             using (var db = new SynapseDbContext())
             {
-                var roomList = db.RoomMemberships
+                var roomList = db.GetMembershipForUser(userId)
                                  .Where(m =>
-                                            m.Membership == "join" &&
-                                            m.UserId == userId)
-                                 .Select(room => GetRoom(room.RoomId));
+                                            m.Membership == "join")
+                                 .Select(room => GetRoom(room.RoomId, false));
                 
                 _userMembershipComplete.Add(userId);
                 return roomList.ToList();
@@ -66,6 +69,31 @@ namespace Matrix.SynapseInterop.Common.MatrixUtils
 
             return res;
         }
+
+        public void FetchLatestEventsForRooms(IEnumerable<string> roomIds, int limit)
+        {
+            using (var db = new SynapseDbContext())
+            {
+                Dictionary<string, Event> eventSet = new Dictionary<string, Event>();
+
+                foreach (var roomId in new HashSet<string>(roomIds))
+                {
+                    var events = db.Events.Where(e => e.RoomId == roomId).OrderByDescending(e => e.StreamOrdering)
+                                   .Take(limit);
+
+                    events.ForEach(ev => eventSet.Add(ev.EventId, ev));
+                }
+                
+                // Get all the json in one go.
+                foreach (var ev in 
+                    db.EventsJson
+                      .Where(ej => eventSet.ContainsKey(ej.EventId))
+                      .Select(ej => new EventJsonSet(eventSet[ej.EventId], ej)).GroupBy(ev => ev.RoomId))
+                {
+                    GetRoom(ev.Key, false).PopulateEventCache(ev);
+                }
+            }
+        }
     }
 
     public class CachedMatrixRoom
@@ -73,11 +101,87 @@ namespace Matrix.SynapseInterop.Common.MatrixUtils
         public readonly string RoomId;
         public List<string> Membership { get; private set; }
         public string[] Hosts { get; private set; }
+        private readonly Dictionary<string, EventJsonSet> _eventCache;
+        private readonly Dictionary<Tuple<string, string>, EventJsonSet> _state;
+        private readonly StateGroupCache _stateGroupCache;
         
         public CachedMatrixRoom(string roomId)
         {
             RoomId = roomId;
             Membership = null;
+            _eventCache = new Dictionary<string, EventJsonSet>();
+            _state = new Dictionary<Tuple<string, string>, EventJsonSet>();
+            _stateGroupCache = new StateGroupCache();
+        }
+
+        public EventJsonSet GetEvent(string eventId)
+        {
+            if (_eventCache.TryGetValue(eventId, out var res))
+            {
+                WorkerMetrics.ReportCacheHit("CachedMatrixRoom.GetEvent");
+                return res;
+            }
+
+            WorkerMetrics.ReportCacheMiss("CachedMatrixRoom.GetEvent");
+
+            using (var db = new SynapseDbContext())
+            {
+                var eventJsonSet = db
+                                  .Events
+                                  .Where(ev => ev.EventId == eventId)
+                                  .Select(ev => new EventJsonSet(ev, null))
+                                  .FirstOrDefault();
+
+                if (eventJsonSet == null) return null;
+                _eventCache.TryAdd(eventId, eventJsonSet);
+                return eventJsonSet;
+            }
+        }
+
+        public IEnumerable<EventJsonSet> GetLatestEvents(int limit)
+        {
+            return _eventCache.Values.OrderByDescending(ev => ev.StreamOrdering).Take(limit);
+        }
+
+        public IEnumerable<EventJsonSet> GetPreviousEvents(string eventId, int limit)
+        {
+            using (var db = new SynapseDbContext())
+            {
+                var ev = GetEvent(eventId);
+
+                return db.Events.Where((e) => e.StreamOrdering <= ev.StreamOrdering && e.RoomId == ev.RoomId)
+                         .OrderByDescending((e) => e.StreamOrdering).Take(limit).Select((e) => GetEvent(e.EventId)).ToList();
+            }
+        }
+
+        public void PopulateEventCache(IEnumerable<EventJsonSet> events)
+        {
+            foreach (var ev in events)
+            {
+                // Events are immutable, so if it conflicts then let it be.
+                _eventCache.TryAdd(ev.EventId, ev);
+            }
+        }
+
+        public void PopulateStateCache()
+        {
+            // We are repopulating the state cache, so drop what we have.
+            _state.Clear();
+
+            using (var db = new SynapseDbContext())
+            {
+                foreach (var stateEvent in db.CrntRoomState.Where(ev => ev.RoomId == RoomId))
+                {
+                    var ev = GetEvent(stateEvent.EventId);
+
+                    _state.Add(new Tuple<string, string>(stateEvent.Type, stateEvent.StateKey), ev);
+                }
+            }
+        }
+
+        public IEnumerable<EventJsonSet> GetStateAtEvent(string eventId)
+        {
+            return _stateGroupCache.GetStateForEvent(eventId).Select((e) => GetEvent(e.EventId));
         }
 
         public void PopulateMemberCache()
@@ -89,5 +193,7 @@ namespace Matrix.SynapseInterop.Common.MatrixUtils
                 Membership = members.ToList();
             }
         }
+
+        public IEnumerable<EventJsonSet> GetCurrentState => _state.Values.ToList();
     }
 }
